d4c4aff104ebb2f18ef07e9f08cccd18b651533a
OoiBP <bpooi0702@gmail.com>
2019-09-05 13:17:27 +0800

    Update music.js

--- music.js
+++ music.js
@@ -4,4 +4,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     music.changeTempoBy(-20)
 })
+input.onButtonPressed(Button.AB, function () {
+    music.stopMelody(MelodyStopOptions.All)
+})
 music.beginMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Forever)
